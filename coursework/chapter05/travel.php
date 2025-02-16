36ad1cf9d635a78d589bac8aa760b1c585b646d3
Keny <dedkeny@pop-os.lan>
2025-02-15 20:20:18 -0500

    diff it out

--- coursework/chapter05/travel.php
+++ coursework/chapter05/travel.php
@@ -24,13 +24,12 @@
 	$hotelCost = 110.00 * ($numTickets * $numNights);
 	$totalCost = $airlineCost + $hotelCost;
 
-	print('<h1>Thank you for your order!</h1> 
-            <p> You are coming with ' . $numTickets .'
-        friends and staying for '. $numNights . ' nights. </p>');
-	print('<p> The cost of your flight will be $' . $airlineCost .'
-        The hotel stay will cost $' . $hotelCost .'
-        Bringing the total cost to $' . $totalCost . '</p>');
-
+	print('<h2>Thank you for your order!</h2> 
+        <p> You are coming with ' . $numTickets .
+	    ' friends and staying for '. $numNights . ' nights.' .
+	    '<br>The cost of your flight will be $' . $airlineCost .
+	    '<br>The hotel stay will cost $' . $hotelCost .
+	    '<br>Bringing the total cost to $' . $totalCost . '</p>');
 	?>
 
 </body>
