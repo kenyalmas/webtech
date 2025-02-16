b4372bf32e0dc3cfb89c7897e1f7996fbac02f28
Keny <dedkeny@pop-os.lan>
2025-02-15 20:00:54 -0500

    diff it out

--- coursework/chapter05/travel.php
+++ coursework/chapter05/travel.php
@@ -17,7 +17,19 @@
 	<h1>Travel to Rome</h1>
 
 	<?php
+        $numTickets = $_POST['numTickets'];
+	$numNights = $_POST['numNights'];
 
+	$airlineCost = 875.00 * $numTickets;
+	$hotelCost = 110.00 * ($numTickets * $numNights);
+	$totalCost = $airlineCost + $hotelCost;
+
+	print('<h1>Thank you for your order!</h1> 
+            <p> You are coming with ' . $numTickets .'
+        friends and staying for '. $numNights . ' nights. </p>');
+	print('<p> The cost of your flight will be $' . $airlineCost .'
+        The hotel stay will cost $' . $hotelCost .'
+        Bringing the total cost to $' . $totalCost . '</p>');
 
 	?>
 
