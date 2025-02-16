c91d5dd0f74b117c82a3a810f10612ddce9235ce
Keny <dedkeny@pop-os.lan>
2025-02-16 14:16:20 -0500

    diff it out

--- coursework/chapter05/event.php
+++ coursework/chapter05/event.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
-<!--	Author: Kenneth Almas
-		Date:   02/11/2025
-		File:	event.php
-		Purpose: Chapter 5 Exercise
+<!--    Author: Kenneth Almas
+        Date:   02/11/2025
+        File:    event.php
+        Purpose: Chapter 5 Exercise
 
 -->
 
 <html>
 <head>
-	<title>Performance</title>
-	<link rel ="stylesheet" type="text/css" href="sample.css">
+    <title>Performance</title>
+    <link rel ="stylesheet" type="text/css" href="sample.css">
 </head>
 
 <body>
-	<?php
-        $firstName = $_POST['firstName'];
-	$phoneNumber = $_POST['phoneNumber'];
-	$numTickets = $_POST['numTickets'];
+    <?php
+    $firstName = $_POST['firstName'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $numTickets = $_POST['numTickets'];
 
-	$ticketCost = $numTickets * 35.00;
-	print('<h1>Thank you for your order ' . $firstName .  '</h1>');
-	print('<p>You bought a total of ' . $numTickets . ' tickets for a total cost of $' . $ticketCost . '</p>');
-	print('<p> Electronic copies will be sent to ' . $phoneNumber . '</p>');
-	?>
+    $ticketCost = $numTickets * 35.00;
+    print('<h1>Thank you for your order ' . $firstName .  '</h1>');
+    print('<p>You bought a total of ' . $numTickets .
+        ' tickets for a total cost of $' . $ticketCost . '</p>');
+    print('<p> Electronic copies will be sent to ' . $phoneNumber . '</p>');
+    ?>
 
 </body>
 </html>
--- coursework/chapter05/software-order.php
+++ coursework/chapter05/software-order.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<!--Author:
-	Date:
+<!--Author: Kenneth Almas
+	Date:   02/16/2025
 	File:	software-order.php
 	Purpose:Chapter 5 Exercise
 
@@ -17,8 +17,19 @@
 	<h1>Your Order</h1>
 
 	<?php
+        $os = $_POST['os'];
+	$numCopies = $_POST['numCopies'];
 
+	$subtotal = $numCopies * 35.00;
+	$tax = $subtotal * 0.07;
+	$shipHandle = numberCopies * 1.25;
+	$total = $subtotal + $tax + $shipHandle;
 
+	print('<p>You ordered ' . $numCopies . ' copies for the ' . $os . ' operating system.<br>' .
+	'The subtotal is $' . $subtotal . '. <br>' .
+	'The tax will be $' . $tax . '.<br>' .
+	'The shipping cost will be $' . $shipHandle . '.<br>' .
+	'Bringing the total cost to $' . $total . '.</p>')
 	?>
 
 </body>
