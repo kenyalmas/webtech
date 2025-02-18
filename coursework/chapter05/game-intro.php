5e81b84689d6a53b6a4e763f19251d612a3b06d2
Keny <dedkeny@pop-os.lan>
2025-02-18 16:50:57 -0500

    diff it out

--- coursework/chapter05/game-intro.php
+++ coursework/chapter05/game-intro.php
@@ -1,23 +1,39 @@
 <!DOCTYPE html>
-<!--Author:
-	Date:
-	File:	game-intro.php
-	Purpose:Chapter 5 Exercise
+<!--Author: Kenneth Almas
+    Date:   02/18/2025
+    File:   game-intro.php
+    Purpose:Chapter 5 Exercise
 
 -->
 
 
 <html>
 <head>
-	<title>game-intro</title>
-	<link rel ="stylesheet" type="text/css" href="sample.css">
+    <title>game-intro</title>
+    <link rel ="stylesheet" type="text/css" href="sample.css">
 </head>
 <body>
 
-	<?php
+    <?php
+    $charName = $_POST['charName'];
+    $charType = $_POST['charType'];
+    $healthTokens = $_POST['healthTokens'];
+    $expTokens = $_POST['expTokens'];
+    $supplyTokens = $_POST['supplyTokens'];
 
+    $healthGold = $healthTokens / 10;
+    $expGold = $expTokens / 2;
+    $supplyGold = $supplyTokens / 25;
+    $totalGold = $expGold + $supplyGold + $healthGold;
 
-	?>
+    print("<h1>Your Character</h1>");
+    print("<p><strong>Character Name:</strong> $charName </p>");
+    print("<p><strong>Character Type:</strong> $charType </p>");
+    print("<p><strong>Health Tokens:</strong> $healthTokens </p>");
+    print("<p><strong>Experience Tokens:</strong> $expTokens </p>");
+    print("<p><strong>Supply Tokens:</strong> $supplyTokens </p>");
+    print("<p>Total Gold spent:<strong> $totalGold </strong></p>");
+    ?>
 
 </body>
 </html>
--- coursework/chapter06/paint-report.php
+++ coursework/chapter06/paint-report.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<!--	Author:
-		Date:
+<!--	Author: Kenneth Almas
+		Date:   02/18/2025
 		File:	paint-report.php
 		Purpose: Chapter 6 Exercise
 		
@@ -16,17 +16,20 @@
 	<h1>MONTHLY REPORT</h1>
 
 	<?php
+        $contracts = fopen("paint-contracts.txt", "r");
+	for ($i = 1;$i <= 5;$i++) {
+	    ${'payment' . $i} = trim(fgets($contracts));
+	}
+	fclose($contracts);
+	$total = $payment1 + $payment2 + $payment3 + $payment4 + $payment5;
 
-
-
-
-		print("<p>INCOME FROM PAINT CONTRACTS:</p><hr>");
-		print("<p>$".number_format($payment1, 2)."<br>");
-		print("$".number_format($payment2, 2)."<br>");
-		print("$".number_format($payment3, 2)."<br>");
-		print("$".number_format($payment4, 2)."<br>");
-		print("$".number_format($payment5, 2)."</p>");
-		print("<hr><p><strong>TOTAL: $".number_format($total, 2)."</strong></p><hr>");
+	print("<p>INCOME FROM PAINT CONTRACTS:</p><hr>");
+	print("<p>$".number_format($payment1, 2)."<br>");
+	print("$".number_format($payment2, 2)."<br>");
+	print("$".number_format($payment3, 2)."<br>");
+	print("$".number_format($payment4, 2)."<br>");
+	print("$".number_format($payment5, 2)."</p>");
+	print("<hr><p><strong>TOTAL: $".number_format($total, 2)."</strong></p><hr>");
 	?>
 
 </body>
