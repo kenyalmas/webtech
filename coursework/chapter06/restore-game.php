2068c37ba3de9713a6b48e209f750bd7692939f1
Keny <dedkeny@pop-os.lan>
2025-02-24 16:25:14 -0500

    diff it out

--- coursework/chapter06/restore-game.php
+++ coursework/chapter06/restore-game.php
@@ -14,9 +14,12 @@
 <body>
     <h1>GAME CHARACTER</h1>
     <?php
-        $saveFile = fopen("game-status.dat", "r");
-    list($charName, $charType, $healthTokens, $expTokens, $supplyTokens)
-        = trim(fgets($saveFile));
+    $saveFile = fopen("game-status.dat", "r");
+    $charName = fgets($saveFile);
+    $charType = fgets($saveFile);
+    $healthTokens = fgets($saveFile);
+    $expTokens = fgets($saveFile);
+    $supplyTokens = fgets($saveFile);
     fclose($saveFile);
 
     print("<p>Your character is <strong>$charName</strong> the <strong>$charType</strong>.</p>");
